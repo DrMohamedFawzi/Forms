c14299ed430c8720fbed9e391ddc30fb410aa55e
DrMohamedFawzi <94241270+DrMohamedFawzi@users.noreply.github.com>
2022-08-09 23:54:50 +0300

    Simple Springboot + thymeleaf

---
+++ Forms/src/test/java/train/Forms/FormsApplicationTests.java
@@ -0,0 +1,13 @@
+package train.Forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
